cda6d87f4b3e2d80ac9eec11e376ec6eb2efc31c
Luc Tielen <luc.tielen@kabisa.nl>
2020-05-12 09:04:11 +0200

    First session: BF parser
    Co-authored-by: Matthijs Groen <matthijs.groen@kabisa.nl>
    Co-authored-by: Luc Zwanenberg <luc.zwanenberg@kabisa.nl>
    Co-authored-by: Egon Meijers <egon.meijers@kabisa.nl>
    Co-authored-by: Guido Theelen <guido.theelen@kabisa.nl>
    Co-authored-by: Arjan De Pooter <arjan.de.pooter@kabisa.nl>
    Co-authored-by: Rene Dohmen <rene@formatics.nl>

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # BF
 
 A brainfuck interpreter, written in Haskell.
+This code was written during a few SIGFP sessions to learn some Haskell, together as a group.
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,89 @@
 
-module Main (main) where
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-unused-matches #-}  -- TODO remove
+
+module Main ( main ) where
+
+import qualified Text.Megaparsec as P
+import Control.Applicative
+import Data.Void
+import Data.Foldable
+
+
+data Instruction
+  = IncrementPointer   -- >
+  | DecrementPointer   -- <
+  | Increment          -- +
+  | Decrement          -- -
+  | WriteOutput        -- .
+  | ReadInput          -- ,
+  | Loop [Instruction] -- [ and ]
+  | Nop                -- all the rest, TODO, remove from AST
+  deriving (Show)
+
+type Stream = String
+type Error = Void
+type Parser a = P.Parsec Error Stream a
+type ParseError = P.ParseErrorBundle Stream Error
+
+parse :: FilePath -> Stream -> Either ParseError [Instruction]
+parse = P.parse parser
+
+parser :: Parser [Instruction]
+parser = do
+  increments <- instructionsParser
+  P.eof
+  pure increments
+
+instructionsParser :: Parser [Instruction]
+instructionsParser = many instructionParser
+
+instructionParser :: Parser Instruction
+instructionParser
+  =  increment
+ <|> decrement
+ <|> incrementPointer
+ <|> decrementPointer
+ <|> writeOutput
+ <|> readInput
+ <|> loop
+ <|> nop
+  where
+    c ==> i = i <$ P.single c
+    increment = '+' ==> Increment
+    decrement = '-' ==> Decrement
+    incrementPointer = '>' ==> IncrementPointer
+    decrementPointer = '<' ==> DecrementPointer
+    writeOutput = '.' ==> WriteOutput
+    readInput = ',' ==> ReadInput
+    loop = Loop <$> P.between (P.single '[') (P.single ']') instructionsParser
+    nop = Nop <$ P.noneOf "+-><.,[]"
+
+
+type Interpreter = IO
+
+interpret :: [Instruction] -> Interpreter ()
+interpret = traverse_ interpretSingle
+
+interpretSingle :: Instruction -> Interpreter ()
+interpretSingle = \case
+{-
+  IncrementPointer -> _
+  DecrementPointer -> _
+  Increment -> _
+  Decrement -> _
+  WriteOutput -> _
+  ReadInput -> _
+  Loop instructions -> _
+-}
+  Nop -> pure ()
+  _ -> print "for next time"
+
 
 main :: IO ()
-main = pure ()
+main = do
+  let filePath = "./src/hello.bf"
+  contents <- readFile filePath
+  case parse filePath contents of
+    Left err -> putStrLn $ P.errorBundlePretty err
+    Right instructions -> interpret instructions
+
